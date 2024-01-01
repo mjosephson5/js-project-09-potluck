191ef477a412856b1894418ebd0396d7fde4fde5
josephson_m <josephson4@yahoo.com>
2023-12-31 17:40:37 -0800

    Added button to assign dishes to guests

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//assign button
+const assignButton = document.querySelector(".assign");
+//guest list and assigned dishes
+const assignedItems = document.querySelector(".assigned-items");
 // invite button
 const addGuestButton = document.querySelector(".invite");
 // label for the invite button
@@ -35,10 +39,30 @@ const addToList = function (guest) {
 const updateGuestCount = function (){
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
-    if (guests.length >= 8) {
+    if (guests.length === 8) {
       addGuestButton.classList.add("hide");
       guestInput.classList.add("hide");
       guestInputLabel.classList.add("hide");
       guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function () {
+    const potLuckItems = ["Potato salad", "Pasta salad", "Wine", "Non-alcoholic drinsk", "Watermelon", "Donuts", "Cheese and Crackers", "Brownies", "Hummus", "Veggies", "Cookies","Sandwiches"];
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for (let guest of allGuests) {
+        let randomPotLuckIndex = Math.floor(Math.random() * potLuckItems.length);
+        let randomPotLuckItem = potLuckItems[randomPotLuckIndex];
+
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotLuckItem}`;
+        assignedItems.append(listItem);
+
+        potLuckItems.splice(randomPotLuckIndex, 1);
+    }
+};
+
+assignButton.addEventListener("click", function(){
+  assignItems();
+  assignButton.disabled = true;
+});
